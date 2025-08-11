0453790463fc74a5c8e71806573955ea6cecc914
khanreformiqo <momdou.khan@reformiqo.com>
2025-08-11 16:20:26 +0000

    update

--- whatsapp/hooks.py
+++ whatsapp/hooks.py
@@ -137,12 +137,12 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-doc_events = {
-	"*": {
-		"after_insert": "whatsapp.webhook.webhook",
+# doc_events = {
+# 	"*": {
+# 		"after_insert": "whatsapp.webhook.webhook",
 
-	}
-}
+# 	}
+# }
 
 # Scheduled Tasks
 # ---------------
